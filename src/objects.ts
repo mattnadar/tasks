04c759e558acfcb57d5b2580aa7594f0a35adf0c
Matthew Nadar <BOGUS@EMAIL>
2024-02-21 22:01:01 -0500

    Finished toShortForm

--- src/objects.ts
+++ src/objects.ts
@@ -56,7 +56,9 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const id = question.id;
+    const shortName = question.name.slice(0, 10);
+    return `${id}: ${shortName}`;
 }
 
 /**
