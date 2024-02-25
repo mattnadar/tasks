d369dff1a77d51656e1182db84f4ca9d7a573630
Matthew Nadar <BOGUS@EMAIL>
2024-02-25 15:33:34 -0500

    finished publishAll

--- src/nested.ts
+++ src/nested.ts
@@ -123,7 +123,12 @@ export function makeAnswers(questions: Question[]): Answer[] {
  * each question is now published, regardless of its previous published status.
  */
 export function publishAll(questions: Question[]): Question[] {
-    return [];
+    return [...questions].map(
+        (question: Question): Question => ({
+            ...question,
+            published: true
+        })
+    );
 }
 
 /***
